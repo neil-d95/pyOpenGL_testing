aec2f993a9f54a45dc760776aec1ce1782259a0f
neil-d95 <neil@thedomaliks.net>
2016-03-05 13:03:55 -0500

    Added whole 10 / half 5 degree hash marks for pitch angle.

--- hsi.py
+++ hsi.py
@@ -1,67 +1,9 @@
 #!/usr/bin/python
 import sys
 import time
-
-# This is statement is required by the build system to query build info
-if __name__ == '__build__':
-    raise Exception
-
-'''
-
-cube.py
-Converted to Python by Jason Petrone 6/00
-
-/*
-* Copyright (c) 1993-1997, Silicon Graphics, Inc.
-* ALL RIGHTS RESERVED
-* Permission to use, copy, modify, and distribute this software for
-* any purpose and without fee is hereby granted, provided that the above
-* copyright notice appear in all copies and that both the copyright notice
-* and this permission notice appear in supporting documentation, and that
-* the name of Silicon Graphics, Inc. not be used in advertising
-* or publicity pertaining to distribution of the software without specific,
-* written prior permission.
-*
-* THE MATERIAL EMBODIED ON THIS SOFTWARE IS PROVIDED TO YOU "AS-IS"
-* AND WITHOUT WARRANTY OF ANY KIND, EXPRESS, IMPLIED OR OTHERWISE,
-* INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY OR
-* FITNESS FOR A PARTICULAR PURPOSE.  IN NO EVENT SHALL SILICON
-* GRAPHICS, INC.  BE LIABLE TO YOU OR ANYONE ELSE FOR ANY DIRECT,
-* SPECIAL, INCIDENTAL, INDIRECT OR CONSEQUENTIAL DAMAGES OF ANY
-* KIND, OR ANY DAMAGES WHATSOEVER, INCLUDING WITHOUT LIMITATION,
-* LOSS OF PROFIT, LOSS OF USE, SAVINGS OR REVENUE, OR THE CLAIMS OF
-* THIRD PARTIES, WHETHER OR NOT SILICON GRAPHICS, INC.  HAS BEEN
-* ADVISED OF THE POSSIBILITY OF SUCH LOSS, HOWEVER CAUSED AND ON
-* ANY THEORY OF LIABILITY, ARISING OUT OF OR IN CONNECTION WITH THE
-* POSSESSION, USE OR PERFORMANCE OF THIS SOFTWARE.
-*
-* US Government Users Restricted Rights
-* Use, duplication, or disclosure by the Government is subject to
-* restrictions set forth in FAR 52.227.19(c)(2) or subparagraph
-* (c)(1)(ii) of the Rights in Technical Data and Computer Software
-* clause at DFARS 252.227-7013 and/or in similar or successor
-* clauses in the FAR or the DOD or NASA FAR Supplement.
-* Unpublished-- rights reserved under the copyright laws of the
-* United States.  Contractor/manufacturer is Silicon Graphics,
-* Inc., 2011 N.  Shoreline Blvd., Mountain View, CA 94039-7311.
-*
-* OpenGL(R) is a registered trademark of Silicon Graphics, Inc.
-*/
-
-'''
-
-#  cube.c
-#  This program demonstrates a single modeling transformation,
-#  glScalef() and a single viewing transformation, gluLookAt().
-#  A wireframe cube is rendered.
-
-
-try:
-    from OpenGL.GLUT import *
-    from OpenGL.GL import *
-    from OpenGL.GLU import *
-except:
-    print("ERROR: PyOpenGL not installed properly.")
+from OpenGL.GLUT import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
 spin = 0.0
 
@@ -89,11 +31,33 @@ def display():
               0.0, 0.0, 0.0,
               0.0, 1.0, 0.0)
     glPushMatrix()
+    # Fixed triangle for roll indication.
+    glBegin(GL_TRIANGLES)
+    glVertex2f(0.0, 1.0)
+    glVertex2f(0.05, 0.95)
+    glVertex2f(-0.05, 0.95)
+    glEnd()
     glRasterPos2f(2.5, 2.5)
     for char in texts:
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ord(char))
     glRectf(1.0, 0.01, -1.0, -0.01)
     glRotatef(spin, 0.0, 0.0, 1.0)
+    # Draw Pitch marks half for 5 degrees whole for 10 degrees
+    num = 0.25
+    while num < 2.0:
+        if num % 0.5 == 0:
+            line_width = 1.0
+        else:
+            line_width = 0.5
+        glBegin(GL_LINES)
+        glVertex2f(line_width, num)
+        glVertex2f(-line_width, num)
+        glEnd()
+        glBegin(GL_LINES)
+        glVertex2f(line_width, -num)
+        glVertex2f(-line_width, -num)
+        glEnd()
+        num += 0.25
     glColor3f(0.0, 0.0, 1.0)
     glRectf(2.0, 2.0, -2.0, 0.0)
     glColor3f(0.5, 0.35, 0.05)
@@ -121,7 +85,7 @@ glutInit(sys.argv)
 glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
 glutInitWindowSize(500, 500)
 glutInitWindowPosition(100, 100)
-glutCreateWindow('cube')
+glutCreateWindow('HSI')
 init()
 glutDisplayFunc(display)
 glutReshapeFunc(reshape)
