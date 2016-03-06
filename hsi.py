3aa39f5b77f5b424245614d81e75c1684dd6869e
neil-d95 <neil@thedomaliks.net>
2016-03-06 14:38:22 -0500

    Added pitch Movement and test for ever 10 degrees

--- hsi.py
+++ hsi.py
@@ -28,6 +28,7 @@ def display():
     # #     spin = spin - 360.0
     text_roll = str(roll)
     text_pitch = str(pitch)
+    font = GLUT_BITMAP_HELVETICA_18
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glColor3f(1.0, 1.0, 1.0)
@@ -44,17 +45,20 @@ def display():
     glVertex2f(-0.05, 0.95)
     glEnd()
     # Roll Text top Right
-    glRasterPos2f(2.0, 2.0)
+    glRasterPos2f(1.5, 2.0)
     for char in text_roll:
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ord(char))
     glRasterPos2f(-2.0, 2.0)
     for char in text_pitch:
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ord(char))
+    glColor3f(1.0, 1.0, 0.0)
     glRectf(1.0, 0.01, -1.0, -0.01)
     glRotatef(roll, 0.0, 0.0, 1.0)
-    glRotatef(pitch, 1.0, 0.0, 0.0)
+    pitch_angle = pitch * 0.05
+    glTranslatef(0.0, pitch_angle, 0.0)
     # Draw Pitch marks half for 5 degrees whole for 10 degrees
-    num = 0.25
+    glColor3f(1.0, 1.0, 1.0)
+    num = 0.0
     while num < 2.0:
         if num % 0.5 == 0:
             line_width = 1.0
@@ -64,6 +68,17 @@ def display():
         glVertex2f(line_width, num)
         glVertex2f(-line_width, num)
         glEnd()
+        # Add 10 degree Text
+        height = (glutBitmapHeight(font) / 100)
+        degree = num / 0.05
+        if degree % 10 == 0 and degree != 0:
+            print(num, height)
+            glRasterPos2f(-1.0, num)
+            for char in str(int(degree)):
+                glutBitmapCharacter(font, ord(char))
+            glRasterPos2f(-1.0, -num)
+            for char in str(int(-degree)):
+                glutBitmapCharacter(font, ord(char))
         glBegin(GL_LINES)
         glVertex2f(line_width, -num)
         glVertex2f(-line_width, -num)
