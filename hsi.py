c952ae4d0a1d05b84d9a43bb1eba12ded4b5c38e
neil-d95 <neil@thedomaliks.net>
2016-03-06 14:45:15 -0500

    Broke up Plane reference line and added Center point. Also Removed print lines.

--- hsi.py
+++ hsi.py
@@ -52,7 +52,9 @@ def display():
     for char in text_pitch:
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ord(char))
     glColor3f(1.0, 1.0, 0.0)
-    glRectf(1.0, 0.01, -1.0, -0.01)
+    glRectf(0.02, 0.02, -0.02, -0.02)
+    glRectf(1.0, 0.01, 0.25, -0.01)
+    glRectf(-0.25, 0.01, -1.0, -0.01)
     glRotatef(roll, 0.0, 0.0, 1.0)
     pitch_angle = pitch * 0.05
     glTranslatef(0.0, pitch_angle, 0.0)
@@ -69,10 +71,8 @@ def display():
         glVertex2f(-line_width, num)
         glEnd()
         # Add 10 degree Text
-        height = (glutBitmapHeight(font) / 100)
         degree = num / 0.05
         if degree % 10 == 0 and degree != 0:
-            print(num, height)
             glRasterPos2f(-1.0, num)
             for char in str(int(degree)):
                 glutBitmapCharacter(font, ord(char))
@@ -103,7 +103,6 @@ def display():
 
 def reshape(w, h):
     glViewport(0, 0, w, h)
-    print(str(w) + " x " + str(h))
     if h == 0:
         h = 1
     aspect = w/h
@@ -120,10 +119,10 @@ def keyboard(key, x, y):
 
     if key == GLUT_KEY_LEFT or key == b"4":
         print("Left")
-        roll += 0.5
+        roll -= 0.5
     elif key == GLUT_KEY_RIGHT or key == b"6":
         print("Right")
-        roll -= 0.5
+        roll += 0.5
     elif key == GLUT_KEY_UP or key == b"8":
         print("Up")
         pitch += 0.5
